f33f0652265aa8986be7ffa9881d9c0e5ad5e203
Yauheni <yauhenbelarus@gmail.com>
2024-08-29 13:55:24 +0300

    Added logger initialazing

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
@@ -10,9 +17,31 @@ func main() {
 
 	fmt.Println(cfg)
 
-	// TODO: инициализировать логгер
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+	)
 
 	// TODO: инициализация приложения (app)
 
 	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
